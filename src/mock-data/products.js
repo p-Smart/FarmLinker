5a8d5b2a120ae5d3fd52d475c6538dbc0c175c30
Kenny-ai <kennysoyoye07@gmail.com>
2023-11-15 10:01:17 +0100

    changed products and product categories array

--- src/mock-data/products.js
+++ src/mock-data/products.js
@@ -1,169 +1,127 @@
 
 
-export const productCategories = ['Vegetables', 'Fruits', 'Food & Drinks', 'Dried Foods']
+export const productCategories = ['Fruits', 'Vegetables', 'Food & Drinks', 'Dried Foods']
 
 
 export const products = [
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Maize',
-        price: '1450',
-        image: '/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Watermelon',
-        price: '1200',
-        image: '/assets/images/18_829b2943-5bb4-4e3e-ac28-668e33e09ee3_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Maize',
-        price: '1450',
-        image: '/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Watermelon',
-        price: '1200',
-        image: '/assets/images/18_829b2943-5bb4-4e3e-ac28-668e33e09ee3_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Maize',
-        price: '1450',
-        image: '/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Watermelon',
-        price: '1200',
-        image: '/assets/images/18_829b2943-5bb4-4e3e-ac28-668e33e09ee3_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Maize',
-        price: '1450',
-        image: '/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Watermelon',
-        price: '1200',
-        image: '/assets/images/18_829b2943-5bb4-4e3e-ac28-668e33e09ee3_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Maize',
-        price: '1450',
-        image: '/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Watermelon',
-        price: '1200',
-        image: '/assets/images/18_829b2943-5bb4-4e3e-ac28-668e33e09ee3_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Maize',
-        price: '1450',
-        image: '/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Watermelon',
-        price: '1200',
-        image: '/assets/images/18_829b2943-5bb4-4e3e-ac28-668e33e09ee3_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Maize',
-        price: '1450',
-        image: '/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Watermelon',
-        price: '1200',
-        image: '/assets/images/18_829b2943-5bb4-4e3e-ac28-668e33e09ee3_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Maize',
-        price: '1450',
-        image: '/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Watermelon',
-        price: '1200',
-        image: '/assets/images/18_829b2943-5bb4-4e3e-ac28-668e33e09ee3_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Maize',
-        price: '1450',
-        image: '/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png',
-        category: 'Vegetables'
-    },
-    {
-        title: 'Banana',
-        price: '1200',
-        image: '/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png',
-        category: 'Vegetables'
-    },
-]
+  {
+    title: "Banana",
+    price: "1200",
+    image: "/assets/images/2_dfb555bf-4369-4213-9642-1cdc34ae98ea_600x.png",
+    category: "Fruits",
+  },
+  {
+    title: "Papaya",
+    price: "1200",
+    image: "/assets/images/18_829b2943-5bb4-4e3e-ac28-668e33e09ee3_600x.png",
+    category: "Fruits",
+  },
+  {
+    title: "Maize",
+    price: "1450",
+    image: "/assets/images/5_acfd9bef-0515-4547-8715-ae9dba6bc0b0_600x.png",
+    category: "Vegetables",
+  },
+  {
+    title: "Berries",
+    price: "1450",
+    image: "/assets/images/berries.webp",
+    category: "Fruits",
+  },
+  {
+    title: "Lettuce",
+    price: "1450",
+    image: "/assets/images/lettuce.webp",
+    category: "Vegetables",
+  },
+  {
+    title: "Avocado",
+    price: "1450",
+    image: "/assets/images/avocado.webp",
+    category: "Fruits",
+  },
+  {
+    title: "Peach",
+    price: "1450",
+    image: "/assets/images/peach.webp",
+    category: "Fruits",
+  },
+  {
+    title: "Radish",
+    price: "1450",
+    image: "/assets/images/radish.webp",
+    category: "Fruits",
+  },
+  {
+    title: "Broccoli",
+    price: "1450",
+    image: "/assets/images/broccoli.webp",
+    category: "Vegetables",
+  },
+  {
+    title: "Coffee grounds",
+    price: "1450",
+    image: "/assets/images/coffee.webp",
+    category: "Food & Drinks",
+  },
+  {
+    title: "Dried Steak",
+    price: "1450",
+    image: "/assets/images/dried-steak.webp",
+    category: "Dried Foods",
+  },
+  {
+    title: "Flour",
+    price: "1450",
+    image: "/assets/images/flour.webp",
+    category: "Food & Drinks",
+  },
+  {
+    title: "Kiwi Snacks",
+    price: "1450",
+    image: "/assets/images/kiwi-snacks.webp",
+    category: "Food & Drinks",
+  },
+  {
+    title: "Onions",
+    price: "1450",
+    image: "/assets/images/onions.webp",
+    category: "Vegetables",
+  },
+  {
+    title: "Pear",
+    price: "1450",
+    image: "/assets/images/pear.webp",
+    category: "Fruits",
+  },
+  {
+    title: "Tomatoes",
+    price: "1450",
+    image: "/assets/images/tomatoes.webp",
+    category: "Vegetables",
+  },
+  {
+    title: "Pomegranate",
+    price: "1450",
+    image: "/assets/images/pomegranate.webp",
+    category: "Fruits",
+  },
+  {
+    title: "Roots",
+    price: "1450",
+    image: "/assets/images/roots.webp",
+    category: "Dried Foods",
+  },
+  {
+    title: "Shea Butter Drink",
+    price: "1450",
+    image: "/assets/images/shea-butter-drink.webp",
+    category: "Dried Foods",
+  },
+  {
+    title: "Edible Tomatoes",
+    price: "1450",
+    image: "/assets/images/plastic-tomatoes.webp",
+    category: "Food & Drinks",
+  },
+];
